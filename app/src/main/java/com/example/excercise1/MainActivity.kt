da98a9a0c904c07c33e577198fccd7d30d0cdad2
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2024-06-23 09:54:50 -0400

    First Page Complete and additional clean-up

--- app/src/main/java/com/example/excercise1/MainActivity.kt
+++ app/src/main/java/com/example/excercise1/MainActivity.kt
@@ -2,15 +2,9 @@ package com.example.excercise1
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.View.OnClickListener
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.excercise1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), OnClickListener {
@@ -22,14 +16,14 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonMain.setOnClickListener(this)
+        //binding.buttonMain.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        val intent = Intent(this, SecondActivity::class.java).apply {
-            putExtra("hello", binding.textViewMain.text.toString())
-            putExtra("part2", 20)
-        }
-        startActivity(intent)
+//        val intent = Intent(this, SecondActivity::class.java).apply {
+//            putExtra("hello", binding.textViewMain.text.toString())
+//            putExtra("part2", 20)
+//        }
+//        startActivity(intent)
     }
 }
